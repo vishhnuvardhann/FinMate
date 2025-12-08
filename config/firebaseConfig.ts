3df7b14281e2b7d37556fa1f689e7103202844c7
vishhnuvardhann <r.vishnuvardhan2007@gmail.com>
2025-12-08 12:26:38 +0530

    Added favicon + SEO + OpenGraph

--- config/firebaseConfig.ts
+++ config/firebaseConfig.ts
@@ -9,14 +9,15 @@ import { getFirestore } from 'firebase/firestore';
 // ------------------------------------------------------------------
 
 export const firebaseConfig = {
-  apiKey: "AIzaSyDidxY8oSQH86s6dTxjce2Ndt06yTySe0U",
-  authDomain: "finmate-b86d0.firebaseapp.com",
-  projectId: "finmate-b86d0",
-  storageBucket: "finmate-b86d0.firebasestorage.app",
-  messagingSenderId: "572089235354",
-  appId: "1:572089235354:web:c5be5e360a8e72e13eec23",
-  measurementId: "G-38G634FKWP"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
+
 
 // Initialize Firebase using Modular Syntax
 const app = initializeApp(firebaseConfig);
